763a834ac794db24340d2a592a9a9af1227b5e78
Zodiac <zodiac@192-168-0-100.rdsnet.ro>
2023-12-07 23:59:39 +0200

    hw3

--- src/main/java/listeners/RetryAnalyzer.java
+++ src/main/java/listeners/RetryAnalyzer.java
@@ -1,9 +1,12 @@
+package listeners;
+
 import org.testng.IRetryAnalyzer;
 import org.testng.ITestResult;
 
 public class RetryAnalyzer implements IRetryAnalyzer {
 
     int counter = 0;
+
     int limit = 2;
 
     @Override
